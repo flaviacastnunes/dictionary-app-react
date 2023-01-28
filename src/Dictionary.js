1a96e76164aea44052abad161591e78b2e5110c3
flaviacastnunes <117353138+flaviacastnunes@users.noreply.github.com>
2023-01-28 10:45:29 +0000

    ãdded axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyword]=useState(null);
+
+    function handleResponse(response) {
+
+    }
     
     function search(event){
         event.preventDefault()
+        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(url).then(handleResponse);
         }
 
     function handleKeywordChange(event){
